54b0b524dc575118982a77f23117a8783277cceb
TanVung <lientanvung2101@gmail.com>
2025-04-07 23:57:31 +0700

    update import

--- middlewares/imageGoogleUpload.js
+++ middlewares/imageGoogleUpload.js
@@ -1,6 +1,5 @@
 import path from "path";
 import multer from "multer";
-import config from "../config/firebaseConfig"
 
 const fileFilter = (req, file, callback) => {
   if (file.mimetype.startsWith("image")) {
