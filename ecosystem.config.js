417fc1860e59fc4dc7e64d983910b42bae1771cd
TanVung <lientanvung2101@gmail.com>
2025-04-08 16:03:37 +0700

    update node.yml

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+export default {
+    apps: [
+      {
+        name: 'shopapp-online',
+        script: './index.js', // ✅ nếu index.js nằm ở thư mục gốc
+        env: {
+          AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+          AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+          BUCKET_NAME: process.env.BUCKET_NAME,
+          CLIENT_URL: process.env.CLIENT_URL,
+          AWS_REGION: process.env.AWS_REGION,
+          SES_FROM_ADDRESS: process.env.SES_FROM_ADDRESS,
+          PORT: process.env.PORT,
+          NODE_ENV: process.env.NODE_ENV
+        }
+      }
+    ]
+  }
+  
