897f07c39c30e0326e1187c2f68f7fd45bfcd260
TanVung <lientanvung2101@gmail.com>
2025-04-08 09:38:58 +0700

    update import

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -1,4 +1,4 @@
-import { Op, where } from "sequelize";
+import { Op } from "sequelize";
 import db from "../models/index.js";
 import { OrderStatus } from "../constants/index.js";
 import moment from "moment";
