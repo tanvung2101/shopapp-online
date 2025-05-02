357bc334ce3625f03a1df2b99812063afd98179b
TanVung <lientanvung2101@gmail.com>
2025-05-02 14:37:45 +0700

    update index.js

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.use(cors({
 
 // Middleware xử lý CORS
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Origin", "https://shopapp-online-nine.vercel.app");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
   res.header(
     "Access-Control-Allow-Headers",
