a3d048b40716557ec6ba43d81e249ee86d51ef96
TanVung <lientanvung2101@gmail.com>
2025-05-02 09:18:55 +0700

    update

--- index.js
+++ index.js
@@ -44,25 +44,31 @@ import cors from"cors";
 
 
 app.use(express.json())
-express.urlencoded({ extended: true })
+app.use(express.urlencoded({ extended: true }));
 // cookie parser
 app.use(cookieParser(process.env.JWT_REFRESH_SECRET));
 
 
-app.use(
-  cors({
-    origin: "http://localhost:5173",
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Origin",
-      "Content-Type",
-      "X-Requested-With",
-      "Authorization",
-      "Accept",
-    ],
-  })
-);
+// app.use(
+//   cors({
+//     origin: "http://localhost:5173",
+//     credentials: true,
+//     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+//     allowedHeaders: [
+//       "Origin",
+//       "Content-Type",
+//       "X-Requested-With",
+//       "Authorization",
+//       "Accept",
+//     ],
+//   })
+// );
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+}));
+
 
 // Middleware xử lý CORS
 app.use((req, res, next) => {
@@ -83,23 +89,23 @@ app.use((req, res, next) => {
 });
 
 
-// app.get("/health", async (req, res) => {
-//   try {
-//     await db.sequelize.authenticate(); // Kiểm tra kết nối database
-//     res
-//       .status(200)
-//       .json({
-//         server: "UP",
-//         database: "UP",
-//         timestamp: new Date().toISOString(),
-//       });
-//   } catch (error) {
-//     console.error("Database connection error:", error);
-//     res
-//       .status(500)
-//       .json({ server: "UP", database: "DOWN", error: error.message });
-//   }
-// });
+app.get("/health", async (req, res) => {
+  try {
+    await db.sequelize.authenticate(); // Kiểm tra kết nối database
+    res
+      .status(200)
+      .json({
+        server: "UP",
+        database: "UP",
+        timestamp: new Date().toISOString(),
+      });
+  } catch (error) {
+    console.error("Database connection error:", error);
+    res
+      .status(500)
+      .json({ server: "UP", database: "DOWN", error: error.message });
+  }
+});
 
 
 
