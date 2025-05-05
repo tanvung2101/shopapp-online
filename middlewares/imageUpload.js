1ec3ee7f562cf8a46059e5e2df03fe1958cbc6fb
TanVung <lientanvung2101@gmail.com>
2025-05-05 08:19:44 +0700

    update

--- middlewares/imageUpload.js
+++ middlewares/imageUpload.js
@@ -1,33 +1,37 @@
-import path from 'path'
-import multer from 'multer'
+import path from 'path';
+import multer from 'multer';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const storage = multer.diskStorage({
   destination: function (req, file, callback) {
-    const destinationPath = path.join(__dirname, "../uploads/");
+    const destinationPath = path.join(__dirname, '../uploads/');
     callback(null, destinationPath);
   },
   filename: function (req, file, callback) {
     const newFileName = `${Date.now()}-${file.originalname}`;
     callback(null, newFileName);
   },
-})
+});
 
-// Cấu hình filter chỉ cho phép file ảnh
 const fileFilter = (req, file, callback) => {
-    if (file.mimetype.startsWith('image')) {
-        callback(null, true)
-    } else {
-        callback(new Error('Chỉ được phép tải lên file ảnh'), false)
-    }
-}
+  if (file.mimetype.startsWith('image')) {
+    callback(null, true);
+  } else {
+    callback(new Error('Chỉ được phép tải lên file ảnh'), false);
+  }
+};
 
 const upload = multer({
   storage,
-    fileFilter,
-    limits: {
-      fieldSize: 1024 * 1024 * 1 // limit 5mb
-  }
+  fileFilter,
+  limits: {
+    fileSize: 1024 * 1024 * 5, // 5MB chứ không phải fieldSize
+  },
 });
 
 export default upload;
-
